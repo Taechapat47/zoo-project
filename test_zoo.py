f67769c5a5a32759ab315f9512fb9d32f7d021fe
Biddikub <93991379+Taechapat47@users.noreply.github.com>
2024-11-27 20:11:56 +0000

    added the test case

--- test_zoo.py
+++ test_zoo.py
@@ -5,13 +5,8 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_ticket_price_invalid_age(self):
-        with self.assertRaises(ValueError):
-            self.zoo.get_ticket_price(-1)
-
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-    
     def test_teen_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(18), 100)
 
